58556b62d6de27e70813a193aa121a9dbceba64d
jjungk <jsk33a@gmail.com>
2023-07-26 15:03:48 +0900

    feat: Member table 초기 설정

--- jpa-basic/src/main/java/jpabasic/Main.java
+++ jpa-basic/src/main/java/jpabasic/Main.java
@@ -1,7 +1,32 @@
 package jpabasic;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import org.h2.mvstore.tx.Transaction;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello JPA!");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+            Member member = new Member(1L, "정하나");
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception exception) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
     }
 }
